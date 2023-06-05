de1d2cca08ac58bc96124cbe982e56ce41ee3e87
Tsukirei <00049419@uca.edu.sv>
2023-06-05 12:58:46 -0600

    Laboratorio 11

---
+++ Laboratorio11/app/src/main/java/com/example/laboratorio11/network/dto/register/RegisterResponse.kt
@@ -0,0 +1,8 @@
+package com.example.laboratorio11.network.dto.register
+
+import com.google.gson.annotations.SerializedName
+
+
+class RegisterResponse (
+    @SerializedName("msg") val message: String
+        )
